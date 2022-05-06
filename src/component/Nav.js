17318eb489bd49d81e1cc14dfd828f2c2f1ba05f
Jahid Hasan <theshakil53@gmail.com>
2022-05-06 12:02:05 +0600

    responsive nav section

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -6,27 +6,28 @@ import john from '../img/john doe.jpg'
 
 function Nav() {
   return (
-    <div className='flex justify-between p-4 bg-slate-50 shadow-lg h-16'>
-      <div className='flex justify-between w-2/5'>
-        <div >
-            <h4 className='font-medium'>Starbucks <FontAwesomeIcon icon = {faCaretDown}/></h4>
-            
-        </div>
-        <div className='border border-solid border-slate-300 flex rounded-xl items-center overflow-x-hidden gap-1 bg-gray-100' >
-            <span className='las la-search'></span>
-            <input className='outline-none '  type="search" placeholder=' Search' />
-        </div>
-      </div>
-
-        <div className='flex items-center gap-2 mr-4'>
-            <span className='  h-5 w-5'> <i className="fa-regular fa-bell"></i> </span>
-            
-            <img  className='w-8 h-8 rounded-2xl' src={john} alt="" />
-            
-            <h4 className='font-medium'>John Doe <FontAwesomeIcon icon = {faCaretDown}/></h4>
-
-        </div>
+    <div className='flex justify-between p-4 bg-slate-50 shadow-lg h-16 w-full sticky top-0 text-xs sm:text-sm'>
+  <div className='flex justify-between w-2/5'>
+    <div >
+        <h4 className='font-medium '>Starbucks <FontAwesomeIcon icon={faCaretDown}/></h4>
+        
     </div>
+    <div className='hidden sm:block  border border-solid border-slate-300 flex rounded-xl items-center overflow-x-hidden gap-1 bg-gray-100' >
+        <span className='las la-search'></span>
+        <input className='outline-none mt-1 '  type="search" placeholder=' Search' />
+    </div>
+  </div>
+
+    <div className='flex items-center gap-2 mr-4'>
+        <span className='h-5 w-5'><i className="fa-regular fa-bell"></i></span>
+        
+        <img  className='w-8 h-8 rounded-2xl' src={john} alt="" />
+        
+        <h4 className='font-medium'>John Doe <FontAwesomeIcon icon={faCaretDown}/></h4>
+
+    </div>
+    
+</div>
   )
 }
 
